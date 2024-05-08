2998417baf29c2430ffe3adf1c1b61a49474581d
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-05-08 20:04:53 -0300

    Add Hero Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './layout/header/header.component';
+import { HeroComponent } from './layout/hero/hero.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HeroComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent], // Agrega el componente AppComponent al array bootstrap
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  label: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  menuItems: MenuItem[] = [
+    { label: 'Sobre mí', link: '/about' },
+    { label: 'Experiencia', link: '/experience' },
+    { label: 'Proyectos', link: '/projects' },
+    { label: 'Contacto', link: '/contact' },
+  ];
 
-  constructor() { }
+  isMenuOpen = false;
 
-  ngOnInit(): void {
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log(this.isMenuOpen);
   }
 
+  constructor() {}
 }
