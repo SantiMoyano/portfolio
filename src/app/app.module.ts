b5f43ffb2515ec11f780780d8bac4ac7add51def
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-05-09 01:29:52 -0300

    Add About Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './layout/header/header.component';
@@ -5,7 +6,7 @@ import { HeroComponent } from './layout/hero/hero.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeroComponent],
+  declarations: [AppComponent, HeaderComponent, HeroComponent, AboutComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent], // Agrega el componente AppComponent al array bootstrap
